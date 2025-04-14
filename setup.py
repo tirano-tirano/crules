c36ea8078afc2296e8e7ac8d7615b0da097cbc99
takashi <tiranotirano888@gmail.com>
2025-04-14 14:28:49 +0900

    Fix README.md path in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 # READMEファイルのパスを取得
-with open("README.md", encoding="utf-8") as f:
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
